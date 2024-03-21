0c6baa26ea0e560dd777a3739a81946f8fed2826
Jon Sagara <jonsagara@gmail.com>
2024-03-21 10:32:14 -0700

    Use pipelining when constructing a new HttpRequestMessage.

--- src/LabelMaker/GitHubApi.fs
+++ src/LabelMaker/GitHubApi.fs
@@ -20,19 +20,27 @@ module GitHubApi =
     let private _httpClient = initHttpClient()
 
     /// Add an Accept header to the request message with the given mediaType.
-    let private addAcceptHeader (requestMsg : HttpRequestMessage) (mediaType : string) =
+    let private addAcceptHeader (mediaType : string) (requestMsg : HttpRequestMessage) =
         requestMsg.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(mediaType))
+        requestMsg
 
     /// Add an authorization header with a bearer token.
-    let private addBearerTokenAuthorizationHeader (requestMsg : HttpRequestMessage) (token : string) =
+    let private addBearerTokenAuthorizationHeader (token : string) (requestMsg : HttpRequestMessage) =
         requestMsg.Headers.Authorization <- new AuthenticationHeaderValue("Bearer", token)
+        requestMsg
+
+    /// Serialize the value and add it to the request body as UTF-8 encoded JSON text.
+    let private addJsonContent<'TValue> (value : 'TValue) (requestMsg : HttpRequestMessage) =
+        requestMsg.Content <- new StringContent(JsonSerializer.Serialize(value), Encoding.UTF8)
+        requestMsg
 
     /// Get all labels from the GitHub repository.
     let private getExistingLabelsAsync (owner : string) (repo : string) (token : string) =
         task {
-            use requestMsg = new HttpRequestMessage(HttpMethod.Get, $"https://api.github.com/repos/{owner}/{repo}/labels?per_page=100")
-            addAcceptHeader requestMsg "application/vnd.github+json"
-            addBearerTokenAuthorizationHeader requestMsg token
+            use requestMsg = 
+                new HttpRequestMessage(HttpMethod.Get, $"https://api.github.com/repos/{owner}/{repo}/labels?per_page=100")
+                |> addAcceptHeader "application/vnd.github+json"
+                |> addBearerTokenAuthorizationHeader token
 
             use! responseMsg = _httpClient.SendAsync(requestMsg)
             let! responseBody = responseMsg.Content.ReadAsStringAsync()
@@ -41,9 +49,10 @@ module GitHubApi =
 
     let private createLabelAsync (owner : string) (repo : string) (token : string) (newLabel : NewLabel) =
         task {
-            use requestMsg = new HttpRequestMessage(HttpMethod.Post, $"https://api.github.com/repos/{owner}/{repo}/labels")
-            addBearerTokenAuthorizationHeader requestMsg token
-            requestMsg.Content <- new StringContent(JsonSerializer.Serialize(newLabel), Encoding.UTF8)
+            use requestMsg = 
+                new HttpRequestMessage(HttpMethod.Post, $"https://api.github.com/repos/{owner}/{repo}/labels")
+                |> addBearerTokenAuthorizationHeader token
+                |> addJsonContent newLabel
 
             use! responseMsg = _httpClient.SendAsync(requestMsg)
             return! responseMsg.Content.ReadAsStringAsync()
