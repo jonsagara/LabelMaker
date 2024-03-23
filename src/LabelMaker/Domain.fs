3d48946cb847f33c45a4922f71cd789efcdd17ae
Jon Sagara <jonsagara@gmail.com>
2024-03-23 15:16:00 -0700

    Removed improvement, which is basically the same thing as enhancement.

--- src/LabelMaker/Domain.fs
+++ src/LabelMaker/Domain.fs
@@ -10,6 +10,5 @@ module Data =
         new NewLabel (name = "priority:3", color = "7A7737", Description = "Medium")
         new NewLabel (name = "priority:4", color = "0AC8A9", Description = "Low")
         new NewLabel (name = "priority:5", color = "4B8DF0", Description = "Lowest")
-        new NewLabel (name = "improvement", color = "A8DBA3", Description = "Improvements to the project")
         |]
 
